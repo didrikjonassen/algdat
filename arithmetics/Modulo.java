0e833f96ad92e6b6d68347ed349061528224b095
Didrik Jonassen <jonassen.didrik@gmail.com>
2013-12-28 23:14:06 +0100

    OMG dat error in the modular exponentiation.

--- arithmetics/Modulo.java
+++ arithmetics/Modulo.java
@@ -4,7 +4,7 @@ public class Modulo {
 
     public static long exp(long n, long k, long p) {
         long result = 1;
-        while (k > 1) {
+        while (k > 0) {
             if ((k & 1) == 1) {
                 result = (result * n) % p;
             }
