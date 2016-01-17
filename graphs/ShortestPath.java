871f08f7d5d4d9833d5d043437d41f9ef7fde156
Didrik Jonassen <jonassen.didrik@gmail.com>
2016-01-17 21:37:38 +0100

    Added storage of parent on bellmanFord, and implemented a check to see if computation is done.

--- graphs/ShortestPath.java
+++ graphs/ShortestPath.java
@@ -1,30 +1,33 @@
-package algdat.graphs;
+package graphs;
 
 public class ShortestPath {
 
     /**
-     * 
-     * @param v
-     *            The number of vertices in the graph
-     * @param e
-     *            The edges in the graph. Assumed to be on the form {{u1, v1,
-     *            c1},{u2, v2, c2},...,{un, vn, cn}} where u and v are vertices
-     *            and c is the cost of going from u to v.
-     * @param s
-     *            The vertex in the graph which the search is done from.
+     * @param v The number of vertices in the graph
+     * @param e The edges in the graph. Assumed to be on the form {{u1, v1,
+     *          c1},{u2, v2, c2},...,{un, vn, cn}} where u and v are vertices
+     *          and c is the cost of going from u to v.
+     * @param s The vertex in the graph which the search is done from.
      * @return The cost of the shortest path from Vertex s to all vertices.
+     * List is on the form {{v0.distance, v0.parent},{v1.distance, v1.parent}...}
      */
-    public static int[] bellmanFord(int v, int[][] e, int s) {
-        int[] best = new int[v];
+    public static int[][] bellmanFord(int v, int[][] e, int s) {
+        int[][] best = new int[v][2];
         for (int i = 0; i < v; i++) {
-            best[i] = Integer.MAX_VALUE/2;
+            best[i][0] = Integer.MAX_VALUE/2;
+            best[i][1] = -1;
         }
-        best[s] = 0;
-        for (int i = 0; i < v; i++) {
-            for (int k = 0; k < e.length; k++) {
-                int cost = best[e[k][0]] + e[k][2];
-                if (cost < best[e[k][1]]) {
-                    best[e[k][1]] = cost;
+        best[s][0] = 0;
+        int cost;
+        boolean changed = true;
+        for (int i = 0; i < v && changed; i++) {
+            changed = false;
+            for (int[] edge : e) {
+                cost = best[edge[0]][0] + edge[2];
+                if (cost < best[edge[1]][0]) {
+                    best[edge[1]][0] = cost;
+                    best[edge[1]][1] = edge[0];
+                    changed = true;
                 }
             }
         }
